32e9bdab1611436c5ebd20383ff531c9932f4797
Ihab A.B. Awad <ihab.awad@gmail.com>
2021-03-27 15:10:15 -0700

    Enter file name from terminal.

--- python/sweep.py
+++ python/sweep.py
@@ -7,11 +7,12 @@ from pressure.scanivalvesource import Source  # Fake Scanivalve data
 from servo.dynamixel import Fixture  # Dummy servo interface
 
 device_name = input('Enter device name (e.g. /dev/ttyUSB0 or COM1): ')
+file_name = input('Enter file name for output (e.g., output.csv): ')
 
 f = Fixture(device_name)
 s = Source()
 
-with open('output.csv', 'w') as of:
+with open(file_name, 'w') as of:
     for alpha in range(-45, 45 + 1, 5):
         for beta in range(-45, 45 + 1, 5):
             f.moveto(alphabeta.alpha_beta_to_az_el([alpha, beta]))
