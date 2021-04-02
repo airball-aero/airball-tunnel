a1cd6b390cca90ed1740958dafc3520ee52caeb6
Ihab A.B. Awad <ihab.awad@gmail.com>
2021-04-01 20:26:12 -0700

    Changes to Dynamixel driver for stable motion.

--- python/servo/dynamixelv2_0.py
+++ python/servo/dynamixelv2_0.py
@@ -1,10 +1,16 @@
 import servo.dynamixel
 import dynamixel_sdk
 import sys
+import time
 
 # Control table address
-ADDR_PRO_TORQUE_ENABLE      = 64
+ADDR_PRO_TORQUE_ENABLE      =  64
+ADDR_PRO_POSITION_D_GAIN    =  80
+ADDR_PRO_POSITION_I_GAIN    =  82
+ADDR_PRO_POSITION_P_GAIN    =  84
+ADDR_PRO_PROFILE_VELOCITY   = 112
 ADDR_PRO_GOAL_POSITION      = 116
+ADDR_PRO_MOVING             = 122
 ADDR_PRO_PRESENT_POSITION   = 132
 
 # Protocol version
@@ -16,6 +22,12 @@ BAUDRATE                    = 57600
 TORQUE_ENABLE               = 1
 TORQUE_DISABLE              = 0
 
+VELOCITY                    = 25
+
+POSITION_P_GAIN             = 1000
+POSITION_I_GAIN             = 1000
+POSITION_D_GAIN             = 100
+
 DXL_MINIMUM_POSITION_VALUE  = 10
 DXL_MAXIMUM_POSITION_VALUE  = 4000
 DXL_MOVING_STATUS_THRESHOLD = 0
@@ -23,12 +35,13 @@ DXL_MOVING_STATUS_THRESHOLD = 0
 ID_AZ                       = 1
 ID_EL                       = 2
 
-MOVING_THRESHOLD            = 10
+MOVING_THRESHOLD            = 5
 
 class Fixture(servo.dynamixel.Fixture):
     
-    def __init__(self, device_name):
+    def __init__(self, device_name, verbose=False):
         super().__init__(device_name)
+        self.__verbose = verbose
         self.__port_handler = dynamixel_sdk.PortHandler(self.__device_name)
         self.__packet_handler = dynamixel_sdk.PacketHandler(PROTOCOL_VERSION)
         if not self.__port_handler.openPort():
@@ -36,16 +49,18 @@ class Fixture(servo.dynamixel.Fixture):
             sys.exit(-1)
         self.__port_handler.setBaudRate(BAUDRATE)
         for id in [ID_AZ, ID_EL]:
-            dxl_comm_result, dxl_error = self.__packet_handler.write1ByteTxRx(
-                self.__port_handler,
-                id,
-                ADDR_PRO_TORQUE_ENABLE,
-                TORQUE_ENABLE)
-            self.handle_result(dxl_comm_result, dxl_error)
+            self.write_4_byte(id, ADDR_PRO_PROFILE_VELOCITY, VELOCITY)
+            self.write_2_byte(id, ADDR_PRO_POSITION_P_GAIN, POSITION_P_GAIN)
+            self.write_2_byte(id, ADDR_PRO_POSITION_I_GAIN, POSITION_I_GAIN)
+            self.write_2_byte(id, ADDR_PRO_POSITION_D_GAIN, POSITION_D_GAIN)
+            self.write_1_byte(id, ADDR_PRO_TORQUE_ENABLE, TORQUE_ENABLE)
     
     def moveto(self, az_el):
-        self.go_motor(ID_AZ, az_el[0])
-        self.go_motor(ID_EL, az_el[1])        
+        counts = list(map(self.count, az_el))
+        self.go_motor(ID_AZ, counts[0])
+        self.go_motor(ID_EL, counts[1])
+        self.await_motor(ID_AZ, counts[0])
+        self.await_motor(ID_EL, counts[1])
 
     def count(self, degrees):
         """Convert angle in degrees to servo counts."""
@@ -63,23 +78,57 @@ class Fixture(servo.dynamixel.Fixture):
         elif dxl_error != 0:
             print('%s' % self.__packet_handler.getRxPacketError(dxl_error))
 
-    def go_motor(self, id, degrees):
-        """Command a motor to slew to an angle and wait for completion.
+    def write_1_byte(self, id, addr, data):
+        dxl_comm_result, dxl_error = self.__packet_handler.write1ByteTxRx(
+            self.__port_handler, id, addr, data)
+        self.handle_result(dxl_comm_result, dxl_error)
+
+    def write_2_byte(self, id, addr, data):
+        dxl_comm_result, dxl_error = self.__packet_handler.write2ByteTxRx(
+            self.__port_handler, id, addr, data)
+        self.handle_result(dxl_comm_result, dxl_error)
+        
+    def write_4_byte(self, id, addr, data):
+        dxl_comm_result, dxl_error = self.__packet_handler.write4ByteTxRx(
+            self.__port_handler, id, addr, data)
+        self.handle_result(dxl_comm_result, dxl_error)
+
+    def read_4_byte(self, id, addr):
+        data, dxl_comm_result, dxl_error = self.__packet_handler.read4ByteTxRx(
+            self.__port_handler, id, addr)
+        self.handle_result(dxl_comm_result, dxl_error)
+        return data
+
+    def read_1_byte(self, id, addr):
+        data, dxl_comm_result, dxl_error = self.__packet_handler.read1ByteTxRx(
+            self.__port_handler, id, addr)
+        self.handle_result(dxl_comm_result, dxl_error)
+        return data
+
+    def log(self, s):
+        if self.__verbose: print(s)
+        
+    def go_motor(self, id, count):
+        """Command a motor to slew to a given count.
         
         Args:
             id: The motor ID on the Dynamixel chain.
-            degrees: The desired angle in degrees.
+            count: The desired count
         """
-        goal = self.count(degrees)
-        dxl_comm_result, dxl_error = self.__packet_handler.write4ByteTxRx(
-            self.__port_handler,
-            id,
-            ADDR_PRO_GOAL_POSITION,
-            goal)
-        self.handle_result(dxl_comm_result, dxl_error)
+        self.log('go_motor(%s, %s)' % (id, count))
+        self.log('  goal = %s' % count)
+        self.write_4_byte(id, ADDR_PRO_GOAL_POSITION, count)
+
+    def await_motor(self, id, count):
+        """Wait for a motor to reach a given count.
+
+        Args:
+            id: The motor ID on the Dynamixel chain.
+            count: The desired count
+        """
         while 1:
-            present, dxl_comm_result, dxl_error = self.__packetHandler.read4ByteTxRx(
-                self.__port_handler, id, ADDR_PRO_PRESENT_POSITION)
-            self.handle_result(dxl_comm_result, dxl_error)
-            if not abs(goal - present) > MOVING_THRESHOLD:
+            present = self.read_4_byte(id, ADDR_PRO_PRESENT_POSITION)
+            delta = abs(count - present)
+            self.log('  %s - Δ = %s' % (id, delta))
+            if not delta > MOVING_THRESHOLD:
                 break
--- python/servo_test.py
+++ python/servo_test.py
@@ -3,18 +3,22 @@
 '''Try out the servo Fixture.'''
 
 import time
+import sys
 
-from servo.dynamixel import Fixture
+from servo.dynamixelv2_0 import Fixture
 
-device_name = input('Enter device name (e.g. /dev/ttyUSB0 or COM1): ')
+if len(sys.argv) > 1:
+     device_name = sys.argv[1]
+else:
+     device_name = input('Enter device name (e.g. /dev/ttyUSB0 or COM1): ')
 
-f = Fixture(device_name)
+f = Fixture(device_name, True)
 
 angles = [
      [ 30,  30],
-     [ 30, -30],
      [-30, -30],
      [-30,  30],
+     [ 30, -30],
 ]
 
 i = 0
@@ -22,4 +26,4 @@ i = 0
 while True:
      f.moveto(angles[i])
      i = (i + 1) % len(angles)
-     time.sleep(1)
+     time.sleep(0.5)
