5efae16fb764ad9b0dfe73b40fcb60209682838b
Ihab A.B. Awad <ihab.awad@gmail.com>
2022-07-03 22:08:29 -0700

    Modified to follow cleaner module format

--- python/pressure/create_calibration.py
+++ python/pressure/create_calibration.py
@@ -1,31 +1,37 @@
 # Create a sensor calibration file
-# Usage:
-#   create_calibration <device_path> <baud_rate> <cal_file_path>
 
 import sys
 from pressure.pressurescanner import PressureScanner
 
 NUMVALUES = 128
 
-device_path = sys.argv[1]
-baud_rate = int(sys.argv[2])
-cal_file_path = sys.argv[3]
-
-scanner = PressureScanner(device_path, baud_rate)
-
 def mean(values):
     return int(round(sum(values) / len(values)))
 
-while True:
-    pressure = float(input('Enter test pressure (^C when done): '))
-    input('Hit Enter to take readings')
-    readings = scanner.read(NUMVALUES)
-    averages = [
-        mean([r[i] for r in readings])
-        for i in range(0, len(readings[0]))
-    ]
-    with open(cal_file_path, 'a') as outfile:
-        print(
-            '%f,%s' %
-            (pressure, ','.join(map(str, averages))),
-            file=outfile)
+########################################################################
+
+if __name__ == '__main__':
+
+    if len(sys.argv) != 4:
+        print('Usage: ' + sys.argv[0] + ' <device_path> <baud_rate> <cal_file_path>')
+        sys.exit(-1)
+
+    device_path = sys.argv[1]
+    baud_rate = int(sys.argv[2])
+    cal_file_path = sys.argv[3]
+
+    scanner = PressureScanner(device_path, baud_rate)
+
+    while True:
+        pressure = float(input('Enter test pressure (^C when done): '))
+        input('Hit Enter to take readings')
+        readings = scanner.read(NUMVALUES)
+        averages = [
+            mean([r[i] for r in readings])
+            for i in range(0, len(readings[0]))
+        ]
+        with open(cal_file_path, 'a') as outfile:
+            print(
+                '%f,%s' %
+                (pressure, ','.join(map(str, averages))),
+                file=outfile)
