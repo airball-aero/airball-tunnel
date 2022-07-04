5627a34fbf070bda4fbfb0f4954d61cf5cf0ddfe
Ihab A.B. Awad <ihab.awad@gmail.com>
2022-07-03 22:36:46 -0700

    Update to new pressure sensor interfaces

--- python/sweep.py
+++ python/sweep.py
@@ -2,23 +2,39 @@
 
 '''Sweep a range of alpha/beta values and write the output to a CSV file.'''
 
+import sys
 import alphabeta
 import time
-from pressure.scanivalvesource import Source  #  Pressure data
-from servo.dynamixelv2_0 import Fixture  #  servo interface
+from servo.dynamixelv2_0 import Fixture
+from pressure.pressurescanner import PressureScanner
 
-device_name = input('Enter device name (e.g. /dev/ttyUSB0 or COM1): ')
-file_name = input('Enter file name for output (e.g., output.csv): ')
+WAIT_TIME=1.0
+NUM_READINGS=100
 
-f = Fixture(device_name)
-s = Source()
+if len(sys.argv) != 4:
+    device_servo = input('Enter servo device name (e.g. /dev/ttyUSB*): ')
+    device_pressure = input('Enter pressure device name (e.g. /dev/ttyUSB*): ')
+    file_name = input('Enter file name for output (e.g., output.csv): ')
+else:
+    device_servo = sys.argv[1]
+    device_pressure = sys.argv[2]
+    file_name = sys.argv[3]
 
-with open(file_name, 'w') as of:
-    for alpha in range(-24, 24 + 1, 6):
-        for beta in range(-24, 24 + 1, 6):
-            f.moveto(alphabeta.alpha_beta_to_az_el([alpha, beta]))
-            time.sleep(2.8)
-            p = s.scan()
-            of.write(
-                ','.join(
-                    map(str, [alpha, beta] + p)) + '\n')
+f = Fixture(device_servo)
+s = PressureScanner(device_pressure)
+
+for alpha in range(-24, 24 + 1, 6):
+    for beta in range(-24, 24 + 1, 6):
+        print('Moving to alpha=%f, beta=%f' % (alpha, beta))
+        az_el = alphabeta.alpha_beta_to_az_el([alpha, beta])
+        f.moveto(az_el)
+        print('Sleeping for %f sec' % WAIT_TIME)
+        time.sleep(WAIT_TIME)
+        print('Reading %f points' % NUM_READINGS)
+        readings = s.read(100)
+        print('Writing')
+        with open(file_name, 'a') as of:
+            for r in readings:
+                of.write(
+                    ','.join(
+                        map(str, [alpha, beta] + r)) + '\n')
